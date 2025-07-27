4b56d3c0c9cba4c866ab11cdf370db207255b92a
paili2 <zxcv7502@naver.com>
2025-07-27 17:10:22 +0900

    style: add light blue neon focus effect to input container

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -2,10 +2,10 @@ import Input from "./listCard/components/Input";
 
 const AddTodo = () => {
   return (
-    <form className="flex gap-3" action="">
+    <form className="flex justify-between" action="">
       <Input></Input>
       <button
-        className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-600 text-2xl text-white font-bold"
+        className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-600 text-2xl text-white font-bold hover:cursor-pointer"
         type="submit"
       >
         +
--- src/components/listCard/components/Input.tsx
+++ src/components/listCard/components/Input.tsx
@@ -1,6 +1,6 @@
 const Input = () => {
   return (
-    <div className="flex items-center basis-9/10 border-none shadow-lg rounded-4xl px-5 py-2 bg-white">
+    <div className="flex items-center basis-9/10 border-none shadow-lg rounded-4xl px-5 py-2 bg-white  transition-all duration-100 focus-within:shadow-[0_0_15px_rgba(59,130,246,0.6)]">
       <input
         type="text"
         className="w-full appearance-none outline-none border-none bg-transparent"
